a315b3964d367a0eafe7f8725b73eddcc56165b0
ppidu <dev@ppiduhn.de>
2025-07-25 20:41:02 +0200

    adds depth to revealed fields and color codes for every value

--- PiSweeper/ViewModels/CellViewModel.cs
+++ PiSweeper/ViewModels/CellViewModel.cs
@@ -5,8 +5,17 @@ public sealed class CellViewModel(int x, int y, int value) : BaseViewModel
     public int X => x;
 
     public int Y => y;
+    
+    public int Value => value;
 
     public bool IsBomb => value == -1;
+
+    public bool IsRevealed 
+    {
+        get => _isRevealed;
+        private set => SetField(ref _isRevealed, value);
+    }
+    private bool _isRevealed = false;
 
     public string Text
     {
@@ -17,6 +26,7 @@ public sealed class CellViewModel(int x, int y, int value) : BaseViewModel
 
     public void RevealValue()
     {
+        IsRevealed = true;
         Text = value.ToString();
     }
 }
